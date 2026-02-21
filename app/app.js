3a8fdfc78ecf72fe8d4ff603293e5215bc377b86
Marco Colombara <colombaramarco21@gmail.com>
2026-02-21 11:21:01 +0100

    Bruto

--- app/app.js
+++ app/app.js
@@ -1,37 +1,6 @@
 const API_BASE = 'http://localhost/api-jwt'; // es: http://localhost/api
 
-const form = document.getElementById('loginForm');
-const output = document.getElementById('output');
-
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    try {
-        const response = await fetch(`${API_BASE}/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password })
-        });
-
-        const data = await response.json();
-
-        if (!response.ok) {
-            throw new Error(data.error || 'Errore di login');
-        }
-
-        // salva JWT
-        localStorage.setItem('token', data.token);
-
-        output.textContent = 'Login effettuato con successo';
-    } catch (err) {
-        output.textContent = err.message;
-    }
-});
+if(!localStorage.getItem("token")) window.location.href = "/logInOrLogUp";
 
 async function getUsers() {
     const token = localStorage.getItem('token');
