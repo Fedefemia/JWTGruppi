becda427f281060aa8de0e7c70f49b8cd9c68de7
CobraIvi11 <119098882+CobraIvi11@users.noreply.github.com>
2026-02-27 10:41:04 +0100

    Update jwtgruppi_db.sql

--- jwtgruppi_db.sql
+++ jwtgruppi_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Feb 21, 2026 alle 11:25
+-- Creato il: Feb 27, 2026 alle 10:40
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -31,11 +31,24 @@ USE `jwtgruppi_db`;
 
 DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE `tasks` (
-                         `id_task` int(11) NOT NULL,
-                         `title` varchar(50) NOT NULL,
-                         `completed` tinyint(1) NOT NULL DEFAULT 0,
-                         `user_id` int(11) NOT NULL
+  `id_task` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `completed` tinyint(1) NOT NULL DEFAULT 0,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `tasks`
+--
+
+INSERT INTO `tasks` (`id_task`, `title`, `completed`, `user_id`) VALUES
+(1, 'prova', 0, 1),
+(2, 'prova', 0, 1),
+(3, 'prova2', 1, 1),
+(4, 'prova6', 0, 5),
+(5, 'prova9', 0, 5),
+(6, 'prova11', 0, 7),
+(7, 'prova13', 1, 7);
 
 -- --------------------------------------------------------
 
@@ -45,10 +58,10 @@ CREATE TABLE `tasks` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-                         `id` int(11) NOT NULL,
-                         `username` varchar(50) NOT NULL,
-                         `password` varchar(255) NOT NULL,
-                         `role` varchar(50) NOT NULL
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -56,7 +69,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
-    (0, 'cobra', '*A4B6157319038724E3560894F7F932C8886EBFCF', 'admin');
+(1, 'cobra', 'cobra', 'admin'),
+(5, 'acosta', '1234', 'user'),
+(7, 'fedemani', '5678', 'user');
 
 --
 -- Indici per le tabelle scaricate
@@ -66,15 +81,31 @@ INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
 -- Indici per le tabelle `tasks`
 --
 ALTER TABLE `tasks`
-    ADD PRIMARY KEY (`id_task`),
+  ADD PRIMARY KEY (`id_task`),
   ADD KEY `user_id` (`user_id`);
 
 --
 -- Indici per le tabelle `users`
 --
 ALTER TABLE `users`
-    ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id_task` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT per la tabella `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
